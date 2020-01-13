1fe4dc0a85661604f50a7217947eb71c6f7e2a72
Meagan Utsey <meagan.utsey@gmail.com>
2020-01-13 02:21:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -14,7 +14,13 @@ class Scraper
   end
 
   def self.scrape_profile_page(profile_url)
-    
+      :twitter=>"http://twitter.com/flatironschool",
+  :linkedin=>"https://www.linkedin.com/in/flatironschool",
+  :github=>"https://github.com/learn-co",
+  :blog=>"http://flatironschool.com",
+  :profile_quote=>"\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
+  :bio=> "I'm a school"
+}
   end
 
 end
